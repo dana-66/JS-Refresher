8d456b2b55d6f57e6b52ef2d539bc87d77bc2fc8
dana-66 <danatarik66@gmail.com>
2024-07-29 21:37:13 +0300

    Revisiting JS: Operators

--- js/app.js
+++ js/app.js
@@ -24,14 +24,31 @@
 // console.log(renaming);
 
 // Variables and Values can be created when needed
-console.log("Hello World!!!");
+// console.log("Hello World!!!");
 
-let userMessage = "Why ?? my value can be re-assigned";
-var x = "my value is x";
-const y = "i cannot be re-assigned";
+// let userMessage = "Why ?? my value can be re-assigned";
+// var x = "my value is x";
+// const y = "i cannot be re-assigned";
 
-console.log(userMessage);
-console.log(x);
-console.log(y);
+// console.log(userMessage);
+// console.log(x);
+// console.log(y);
 
 // Operators
+console.log(6 + 2);
+console.log("text 1 " + " ,Text 2"); //string concatenation
+console.log(6 - 2);
+console.log(6 / 2);
+console.log(6 * 2);
+console.log(6 % 2);
+console.log(6 ^ 2);
+
+// comparison operators
+console.log(6 === 2); //strictly equal
+console.log(6 > 2);
+console.log(6 < 2);
+console.log(6 >= 2);
+console.log(6 <= 2);
+console.log(6 !== 2);
+
+
